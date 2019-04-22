82720e75d975f8c24b4d2b5e2fa8056185fcaa80
chrysler <chrysler_12@hotmail.es>
2019-04-22 12:45:43 +0200

    parte grafica

---
+++ app/src/main/java/com/example/chrysler_munoz/proyecto/Carta.java
@@ -0,0 +1,14 @@
+package com.example.chrysler_munoz.proyecto;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Carta extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carta);
+    }
+
+}
--- app/src/main/java/com/example/chrysler_munoz/proyecto/MainActivity.java
+++ app/src/main/java/com/example/chrysler_munoz/proyecto/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.chrysler_munoz.proyecto;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        Intent intent;
+        switch(v.getId()){
+            case R.id.btnMenus:
+                intent = new Intent(this,Menus.class);
+                startActivity(intent);
+                break;
+            case R.id.btnProductos:
+                intent = new Intent(this,Carta.class);
+                startActivity(intent);
+                break;
+                default:
+                    return;
 
+        }
     }
 }
