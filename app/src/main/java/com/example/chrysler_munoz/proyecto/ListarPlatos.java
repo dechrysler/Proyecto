e12c1722c9984678c6b98189e2377af709fd8816
chrysler <chrysler_12@hotmail.es>
2019-04-23 18:10:00 +0200

    añadiendo cosas

---
+++ app/src/main/java/com/example/chrysler_munoz/proyecto/ListarPlatos.java
@@ -0,0 +1,13 @@
+package com.example.chrysler_munoz.proyecto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListarPlatos extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_platos);
+    }
+}
